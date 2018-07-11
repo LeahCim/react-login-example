0bb87371e1c19462bf3f41a751de48b92070282e
Michael <bogus739-github@yahoo.de>
2018-07-11 17:17:35 +0100

    Clean up api.js

--- src/api.js
+++ src/api.js
@@ -1,25 +1,21 @@
 export default class Api {
-    static authenticate(username, password) {
-        const LOGIN_URI = 'https://example.com/login';
-        const data = {
-            username,
-            password
-        };
+    // static authenticate(username, password) {
+    //     const LOGIN_URI = 'https://example.com/login';
+    //     const data = {
+    //         username,
+    //         password
+    //     };
 
-        return fetch(
-            LOGIN_URI,
-            {
-                method: "POST",
-                //mode: "cors", // no-cors, cors, *same-origin
-                //credentials: "same-origin", // include, same-origin, *omit
-                headers: {
-                    "Content-Type": "application/json; charset=utf-8"
-                },
-                //redirect: "follow", // manual, *follow, error
-                // referrer: "no-referrer", // no-referrer, *client
-                body: JSON.stringify(data) // body data type must match "Content-Type" header
-            });
-    }
+    //     return fetch(
+    //         LOGIN_URI,
+    //         {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json; charset=utf-8"
+    //             },
+    //             body: JSON.stringify(data) // body data type must match "Content-Type" header
+    //         });
+    // }
 
     static async getData(credentials) {
         const DATA_URI = 'https://example.com/data';
@@ -28,15 +24,10 @@ export default class Api {
             DATA_URI,
             {
                 method: "GET",
-                //mode: "cors", // no-cors, cors, *same-origin
-                //credentials: "same-origin", // include, same-origin, *omit
                 headers: {
                     "Content-Type": "application/json; charset=utf-8",
                     "Authorization": `Basic ${credentials}`
                 }
-                //redirect: "follow", // manual, *follow, error
-                // referrer: "no-referrer", // no-referrer, *client
-                // body: JSON.stringify(data) // body data type must match "Content-Type" header
             });
 
         if (response.status === 200)
